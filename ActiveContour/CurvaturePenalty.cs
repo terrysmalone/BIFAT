a4e5b5d0a8f1df6fe5fa01866f1f64ea71a80619
terrysmalone <terrysmalone@hotmail.com>
2017-06-04 21:38:48 +0100

    Refactored CurvaturePenalty
    I'm going through all code with resharper

--- ActiveContour/CurvaturePenalty.cs
+++ ActiveContour/CurvaturePenalty.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Drawing;
 
 namespace ActiveContour
@@ -11,10 +9,7 @@ namespace ActiveContour
     /// </summary>
     public class CurvaturePenalty
     {
-        private double[,] sdf;
-        private List<Point> narrowBand;
-
-        private double[,] curvaturePenalty;
+        private readonly double[,] _curvaturePenalty;
 
         /// <summary>
         /// Constructor
@@ -23,66 +18,66 @@ namespace ActiveContour
         /// <param name="narrowBand">A list of points that are within the curves narrow band</param>
         public CurvaturePenalty(double[,] sdf, List<Point> narrowBand)
         {
-            this.sdf = sdf;
-            this.narrowBand = narrowBand;
+            var width = sdf.GetLength(0);
+            var height = sdf.GetLength(1);
 
-            int width = sdf.GetLength(0);
-            int height = sdf.GetLength(1);
-
-            curvaturePenalty = new double[width, height];
+            _curvaturePenalty = new double[width, height];
 
             foreach (Point point in narrowBand)
             {
-                int x = point.X;
-                int y = point.Y;
+                var x = point.X;
+                var y = point.Y;
 
-                int left = x - 1;
+                var left = x - 1;
 
                 if (x == 0)
                     left = 0;
 
-                int right = x + 1;
+                var right = x + 1;
 
                 if (x == width - 1)
                     right = width - 1;
 
-                int up = y - 1;
+                var up = y - 1;
 
                 if (y == 0)
                     up = 0;
 
-                int down = y + 1;
+                var down = y + 1;
 
                 if (y == height - 1)
                     down = height - 1;
 
 
                 //Get central derivatives of SDF at x,y
-                double sdfLeft = sdf[left, y];
-                double sdfRight = sdf[right, y];
-                double sdfUp = sdf[x, up];
-                double sdfDown = sdf[x, down];
+                var sdfLeft = sdf[left, y];
+                var sdfRight = sdf[right, y];
+                var sdfUp = sdf[x, up];
+                var sdfDown = sdf[x, down];
 
-                double sdfCentral = (2 * sdf[x, y]);
+                var sdfCentral = (2 * sdf[x, y]);
 
-                double sdfX = -sdfLeft + sdfRight;
-                double sdfY = -sdfDown + sdfUp;
+                var sdfX = -sdfLeft + sdfRight;
+                var sdfY = -sdfDown + sdfUp;
 
-                double sdfXX = sdfLeft - sdfCentral + sdfRight;
-                double sdfYY = sdfDown - sdfCentral + sdfUp;
+                var sdfXx = sdfLeft - sdfCentral + sdfRight;
+                var sdfYy = sdfDown - sdfCentral + sdfUp;
 
-                double sdfXY = -0.25 * sdf[left, down] - 0.25 * sdf[right, up] + +0.25 * sdf[right, down] + 0.25 * sdf[left, up];
+                var sdfXy = -0.25 * sdf[left, down] - 0.25 * sdf[right, up] + 0.25 
+                               * sdf[right, down] + 0.25 * sdf[left, up];
 
-                double sdfX2 = Math.Pow(sdfX, 2);
-                double sdfY2 = Math.Pow(sdfY, 2);
+                var sdfX2 = Math.Pow(sdfX, 2);
+                var sdfY2 = Math.Pow(sdfY, 2);
 
-                curvaturePenalty[x, y] = (((sdfX2 * sdfYY) + (sdfY2 * sdfXX) - (2 * sdfX * sdfY * sdfXY)) / Math.Pow((sdfX2 + sdfY2 + double.Epsilon), 2)) * Math.Pow((sdfX2 + sdfY2), 0.5);
+                _curvaturePenalty[x, y] = (((sdfX2 * sdfYy) + (sdfY2 * sdfXx) - (2 * sdfX * sdfY * sdfXy)) 
+                                            / Math.Pow((sdfX2 + sdfY2 + double.Epsilon), 2)) 
+                                          * Math.Pow((sdfX2 + sdfY2), 0.5);
             }
         }
 
         public double[,] GetCurvaturePenalty()
         {
-            return curvaturePenalty;
+            return _curvaturePenalty;
         }
     }
 }
