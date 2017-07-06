0d62168eac37065c2e31c84748574fab85076af3
terrysmalone <terry.malone@red-gate.com>
2017-07-06 16:32:55 +0100

    Changed MeasurementTranslator to DepthMeasurementHandler

---
+++ BoreholeFeatures/DepthMeasurementHandler.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace BoreholeFeatures
+{
+    internal sealed class DepthMeasurementHandler
+    {
+        private int m_SourceStartDepthInMillimetres;
+        private int m_DepthResolution;
+
+        private int m_StartDepthInPixels;
+        private int m_EndDepthInPixels;
+
+        #region properties
+        
+        internal int SourceStartDepthInMillimetres
+        {
+            get => m_SourceStartDepthInMillimetres;
+
+            set
+            {
+                m_SourceStartDepthInMillimetres = value;
+
+                UpdateDepths();
+            }
+        }
+
+        internal int DepthResolution
+        {
+            get => m_DepthResolution;
+
+            set
+            {
+                m_DepthResolution = value;
+
+                UpdateDepths();
+            }
+        }
+
+        internal int StartDepthInPixels
+        {
+            get => m_StartDepthInPixels;
+
+            set
+            {
+                m_StartDepthInPixels = value;
+
+                UpdateDepths();
+            }
+        }
+
+        internal int EndDepthInPixels
+        {
+            get => m_EndDepthInPixels;
+
+            set
+            {
+                m_EndDepthInPixels = value;
+
+                UpdateDepths();
+            }
+        }
+
+        internal int StartDepthInMillimetres { get; private set; }
+        
+        internal int EndDepthInMillimetres { get; private set; }
+        
+        #endregion properties
+
+        internal DepthMeasurementHandler(int depthResolution)
+        {
+            m_DepthResolution = depthResolution;
+
+            UpdateDepths();
+        }
+
+        private void UpdateDepths()
+        {
+            StartDepthInMillimetres = Convert.ToInt32(m_SourceStartDepthInMillimetres 
+                                                      + m_StartDepthInPixels 
+                                                      * (double)m_DepthResolution);
+
+            EndDepthInMillimetres = Convert.ToInt32(m_SourceStartDepthInMillimetres 
+                                                      + m_EndDepthInPixels 
+                                                      * (double)m_DepthResolution);
+
+
+            
+        }
+    }
+}
